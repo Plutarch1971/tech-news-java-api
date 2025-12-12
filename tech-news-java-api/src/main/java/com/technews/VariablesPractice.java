796747f3329293c0a1a2dfcdb5bc537a744f8abb
Matt-Verni <matthewpmendez@gmail.com>
2025-12-11 19:15:50 -0500

    Added User,Post, Comment  & Vote Classes

---
+++ tech-news-java-api/src/main/java/com/technews/VariablesPractice.java
@@ -0,0 +1,22 @@
+package com.technews;
+
+public class VariablesPractice {
+    public static void main(String[] args) {
+        int a = 4;
+        double b = 3.5;
+        String c = "Cat";
+
+        //Calclulated value is a double;
+        double sum = a +b;
+
+        //Calculated variable contcat is a String
+        String concat = a + c;
+
+        //Value of sum is 7.5
+        System.out.println(sum);
+
+        //Value of concat is 4Cat
+        System.out.println(concat);
+
+    }
+}
